86ba035e78d85ad7a3f67a506bc1fac35a803f21
Dylan Cody Moore <dylan.moore@section6.tech>
2022-01-09 16:03:20 +0900

    Added walls around powerplant and added a network of linked empties to use for destinations in player navigation.

---
+++ Assets/Scripts/LinkedPath.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(fileName = "LinkedPathItem", menuName = "Paths/LinkedPath", order = 0)]
+public class LinkedPath : MonoBehaviour 
+{
+    public GameObject Up, Left, Right, Down;
+}
