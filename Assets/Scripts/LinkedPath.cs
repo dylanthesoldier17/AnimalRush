bf04917f2083ea441ab0e654552dd630d709b0ac
Dylan Cody Moore <dylan.moore@section6.tech>
2022-01-09 19:01:57 +0900

    Added NavMesh movement with LinkedPaths class. Character movement needs fine tuned or an alternative movement method setup like Translate.

--- Assets/Scripts/LinkedPath.cs
+++ Assets/Scripts/LinkedPath.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 //[CreateAssetMenu(fileName = "LinkedPathItem", menuName = "Paths/LinkedPath", order = 0)]
 public class LinkedPath : MonoBehaviour 
 {
-    [field: SerializeField] private GameObject Up { get; set; }
-    [field: SerializeField] private GameObject Left { get; set; }
-    [field: SerializeField] private GameObject Right { get; set; }
-    [field: SerializeField] private GameObject Down { get; set; }
+    [field: SerializeField] public LinkedPath Up { get; set; }
+    [field: SerializeField] public LinkedPath Left { get; set; }
+    [field: SerializeField] public LinkedPath Right { get; set; }
+    [field: SerializeField] public LinkedPath Down { get; set; }
 }
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.AI;
 
 public class MovementController : MonoBehaviour
 {
-    public GameObject startingPoint;
+    public NavMeshAgent navMeshAgent;
 
-    private void Awake() 
+    public LinkedPath location;
+
+    private void Awake()
     {
-    //Set start position based on StartingPoint game object.
-        transform.position = startingPoint.transform.position;
+        //Set start position based on location game object.
+        //transform.position = location.transform.position;
+        navMeshAgent.Warp(location.transform.position);
     }
 
-    private void Update() 
+    private void Update()
     {
-    //Check for user input and move character
-           
+        //Don't do anything if player is navigating to the destination
+        if (navMeshAgent.pathStatus != NavMeshPathStatus.PathComplete) { return; }
+
+        //Check for user input and move character
+        checkInputAndSetLocation(KeyCode.UpArrow, location.Up);
+        checkInputAndSetLocation(KeyCode.LeftArrow, location.Left);
+        checkInputAndSetLocation(KeyCode.RightArrow, location.Right);
+        checkInputAndSetLocation(KeyCode.DownArrow, location.Down);
+    }
+
+    private void checkInputAndSetLocation(KeyCode keyCode, LinkedPath selectedLocation)
+    {
+        if (Input.GetKeyDown(keyCode) && location != null)
+        {
+            location = selectedLocation;
+            navMeshAgent.SetDestination(location.transform.position);
+        }
     }
 }
