6d449cf5ee62a8ea1cb2ee0751f71cf37728b2c6
Dylan Cody Moore <dylan.moore@section6.tech>
2022-01-10 16:42:40 +0900

    Reogranized scripts folder.

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using ScriptableObjectArchitecture;
 
 /* Used "How to make a Health Bar in Unity!" by Brackeys at [https://www.youtube.com/watch?v=BLfNP4Sc_iA] */
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
     public Image fill;
+    public IntVariable playerHealth; // Setup ScriptableObject events for on change update UI.
     public int maxValue = 100;
 
     private void Start()
