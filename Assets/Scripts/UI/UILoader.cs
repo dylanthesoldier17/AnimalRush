75b7f8281dcd74cb19bb03e3ec80244aef60c702
Dylan Cody Moore <dylan.moore@section6.tech>
2022-01-12 13:50:50 +0900

    Removed unused NavMeshs.
    Made UILoader capable to load and unload scene and added GameEventListners. Event Listeners will load and unload UI.
    
    Added game event listener to Particle System in the Cooling Tower. On game start event it'll enable the particles.

--- Assets/Scripts/UI/UILoader.cs
+++ Assets/Scripts/UI/UILoader.cs
@@ -7,9 +7,34 @@ using UnityEngine.SceneManagement;
 public class UILoader : MonoBehaviour
 {
     public StringVariable sceneName;
+    public LoadSceneMode sceneLoadMode = LoadSceneMode.Additive;
+    public LoadOnState loadOnState = LoadOnState.NONE;
 
-    private void Awake() 
+    private void Awake()
     {
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        if(loadOnState == LoadOnState.AWAKE)
+        {
+            loadScene();
+        }
     }
+
+    private void OnEnable() 
+    {
+        if(loadOnState == LoadOnState.ENABLE)
+        {
+            loadScene();
+        }    
+    }
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneName, sceneLoadMode);
+    }
+
+    public void unloadScene()
+    {
+        SceneManager.UnloadScene(sceneName);
+    }
+
+    public enum LoadOnState { NONE, AWAKE, ENABLE }
 }
