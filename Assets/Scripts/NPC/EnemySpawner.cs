51ce396a0e88d84354a73217c2195a5fee818ade
Dylan Cody Moore <dylan.moore@section6.tech>
2022-01-12 13:16:32 +0900

    Setup Enemy Spawner with a SO GameObject Collectionto load in enemies dynaimcally.

--- Assets/Scripts/NPC/EnemySpawner.cs
+++ Assets/Scripts/NPC/EnemySpawner.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ScriptableObjectArchitecture;
 
 public class EnemySpawner : MonoBehaviour
 {
     public float xLeftBounds, xRightBounds;
     public float yPosition, zPosition;
-    public GameObject[] enemies;
+    public GameObjectCollection enemiesCollection;
     public float initialSpawnDelayInSeconds = 3;
     public float spawnRateInSeconds = 1;
     public bool enableSpawner = false;
@@ -14,6 +15,7 @@ public class EnemySpawner : MonoBehaviour
 
     private void Start() 
     {
+        // Create Timer to spawn enemy
         InvokeRepeating("spawnEnemy", initialSpawnDelayInSeconds, spawnRateInSeconds);
     }
 
@@ -21,10 +23,24 @@ public class EnemySpawner : MonoBehaviour
     {
         if(enableSpawner)
         {
-            int index = Random.Range(0, enemies.Length);
+            int index = Random.Range(0, enemiesCollection.Count);
             float randomXPosition = Random.Range(xLeftBounds, xRightBounds); 
-            Instantiate(enemies[index], new Vector3(randomXPosition, yPosition, zPosition), startingRotation);
+            Instantiate(enemiesCollection[index], new Vector3(randomXPosition, yPosition, zPosition), startingRotation);
         }
         
     }
+
+    #region eventable_functions
+
+    public void startSpawner()
+    {
+        enableSpawner = true;
+    }
+
+    public void stopSpawner()
+    {
+        enableSpawner = false;
+    }
+
+    #endregion eventable_functions
 }
